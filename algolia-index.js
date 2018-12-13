c044f60e9cb2c80b917343e71911f34284bc73c5
Jeet Gajjar <j211668@gmail.com>
2018-12-13 08:19:44 -0500

    changes

---
+++ algolia-index.js
@@ -0,0 +1,60 @@
+const algoliasearch = require("algoliasearch");
+const algoliaClient = algoliasearch(process.env.APPLICATIONID, process.env.ALGOLIA_API_KEY);
+const bodyParser = require('body-parser');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWSAPI_KEY);
+
+
+function pingNewsApi(searchItem, todayDate) {
+    newsapi.v2.everything({
+      q: searchItem,
+      from: '2018-11-09', //Change this date to at most 30 days in the 
+      to: todayDate,
+      language: 'en',
+      sortBy: 'relevancy',
+      page: 5
+    }).then(response => {
+      console.log("DONE PINGING!!!!");
+      // console.log(response);
+    });
+  }
+
+
+ function buildIndex(searchItem, todayDate) {
+    let articles = pingNewsApi(searchItem, todayDate);
+  
+    //initialize our index of articles
+    let index = algoliaClient.initIndex("articles");
+  
+    //config index
+    index.setSettings(
+      {
+        searchableAttributes: ["title"]
+      },
+      function(err, content) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(content);
+        }
+      }
+    );
+  
+    index.addObject(articles, function(err, content) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(content);
+      }
+    })
+    
+    return index;
+  }
+
+  function search_algolia(searchParams) {
+
+  }
+
+module.exports = {
+    buildIndex
+  };
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI(process.env.NEWSAPI_KEY);
+const algoliasearch = require("algoliasearch");
+const algoliaClient = algoliasearch(process.env.APPLICATIONID, process.env.ALGOLIA_API_KEY);
+const indexer = require('./algolia-index.js');
+
+// let index = algoliaClient.initIndex('articles');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,10 +19,55 @@ app.get('/', function (req, res) {
 })
 
 app.post('/', function (req, res) {
-  console.log(req.body.article);
+
+  let searchItem = req.body.article;
+  var todayDate = new Date().toISOString().slice(0,10);
+  pingNewsApi(searchItem, todayDate);
+  console.log(search(searchItem));
+  // request(function(err, response, body) {
+  //   if(err) {
+  //     res.render('index', {article: null, error: "Hmmm, try that again please."})
+  //   } else {
+  //     let results = JSON.parse(body)
+  //   }
+  // })
   res.render('index');
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+ function pingNewsApi(searchItem, todayDate) {
+  newsapi.v2.everything({
+    q: searchItem,
+    from: '2018-11-15', //Change this date to at most 30 days in the past
+    to: todayDate,
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 5
+  }).then(response => {
+    console.log("DONE PINGING!!!!");
+    // console.log(response);
+    indexer.buildIndex(response);
+  });
+}
+
+
+function search(article){
+
+  var index = indexer.buildIndex();
+    index.search({
+      query: 'author',
+      attributesToRetrieve: ['title', 'url', 'author']
+    }, 
+    function searchDone(err, content) {
+      if (err) {
+        console.log('error');
+        throw err;
+      }
+      console.log('no error')
+      console.log(content);
+    }
+  );
+}
